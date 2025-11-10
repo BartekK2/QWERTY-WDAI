17a012bd60035bb0c8926717ea3059293b4f1581
Bartek Król <barteksp82@gmail.com>
2025-11-10 23:40:09 +0100

    poprawiono url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-const basename = process.env.NODE_ENV === "production" ? "/QWERTY" : "/";
+const basename = process.env.NODE_ENV === "production" ? "/QWERTY-WDAI" : "/";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename={basename}>
