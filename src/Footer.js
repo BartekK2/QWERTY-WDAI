4108f0c81438847c1d8ed2f168fb20e1c8be3543
Bartek Król <barteksp82@gmail.com>
2025-11-10 23:41:58 +0100

    poprawiono url w stopce

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ function Test() {
         <BreakPoint/>
         <footer>
             <p>Strona tworzona w ramach kursu WDAI (projekt pierwszy) - Bartłomiej Król</p>
-            <a href='https://github.com/BartekK2/QWERTY'>
+            <a href='https://github.com/BartekK2/QWERTY-WDAI'>
             <div id="github-link">
             <img src={GitIcon} style={{height:"40px"}}></img>
             <p>Link do repo</p>
